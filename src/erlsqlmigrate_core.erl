5dc76a6aba14c4626c213c1d848235ec99f9e0a8
Aaron Spiegel <spiegela@gmail.com>
2015-01-13 16:14:21 -0600

    Create migrations-tabl automatically on up (if it doesn't exist)

--- src/erlsqlmigrate_core.erl
+++ src/erlsqlmigrate_core.erl
@@ -60,6 +60,7 @@ up([{Driver,_ConnArgs}]=Config, MigDir, Name) ->
     %% I think its sorted but anyway
     Files = lists:sort(filelib:wildcard(?UPDIR(MigDir,Driver)++"/"++Regex)),
     Migrations = get_migrations(Driver, MigDir, Files),
+    run_driver(Config,create,[MigDir,Name]),
     run_driver(Config, up, Migrations).
 
 %% @spec down([{DB,ConnArgs}], MigDir, Name) -> ok
